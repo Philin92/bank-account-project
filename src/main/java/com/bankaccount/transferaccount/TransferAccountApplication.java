e02677e40605a8b99e72bfb7cc87a74ae25bebbe
Philin92 <someEmail@gmail.com>
2021-01-06 23:48:35 +0300

    first commit

---
+++ src/main/java/com/bankaccount/transferaccount/TransferAccountApplication.java
@@ -0,0 +1,13 @@
+package com.bankaccount.transferaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bankaccount/transferaccount/TransferAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankaccount.transferaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
